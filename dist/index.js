b3411ef1585ccddc07d9190b9dd8eac3b7cf999b
vuongductuanktmt <anhtuanpro312@gmail.com>
2020-08-28 17:34:57 +0700

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 import RichTextEditor from "./RichTextEditor";
+import RichTextToolbar from "./RichTextToolbar";
 import { actions } from "./constants";
-export { RichTextEditor, actions, };
+export { RichTextEditor, actions, RichTextToolbar };
